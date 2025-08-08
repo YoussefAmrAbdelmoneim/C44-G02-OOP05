42f24e33484746e477c9481c5ba4604f56b89a4a
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-08-08 10:24:18 +0300

    Add project files.

---
+++ Demo session 5/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo_session_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
