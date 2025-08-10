bdcfad742e0f86c64c02bd32832aa20aa4408eb6
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-08-10 08:55:23 +0300

    Interface examples

--- Demo session 5/Program.cs
+++ Demo session 5/Program.cs
@@ -23,6 +23,50 @@
             //typeObj.MyMethod(); // Hello From Type
             //					//typeObj.Print(); // Invalid 
             #endregion
+            #region Interface Example 01
+            //TypeA typeA = new TypeA();
+            //PrintFiveNumbersFromSeries(typeA); // 0 2 4 6 8
+
+            //Console.WriteLine("======================");
+
+            //TypeB typeB = new TypeB();
+            //PrintFiveNumbersFromSeries(typeB); // 0 3 6 9 12 
+
+            //TypeC typeC = new TypeC();
+            //PrintFiveNumbersFromSeries(typeC); // Invalid [Class Doesn't Implement Interface]
+
+            #endregion
+            #region Interface Example 02
+
+            //Car carObj = new Car();
+            //carObj.Speed = 120;
+            //carObj.Backward(); // Move Car Backward
+            //carObj.Forward();  // Move Car Forward
+            //carObj.Left();     // Move Car Left
+            //carObj.Right();    // Move Car Right
+
+
+            //Airplane airplaneObj = new Airplane();
+            ////airplaneObj.Backward(); // Invalid
+            //airplaneObj.Forward();  // Airplane Move Forward
+            ////airplaneObj.Left();    // Invalid
+            ////airplaneObj.Right();    // Invalid
+
+
+            //IMoveOnAir moveAirplaneOnAir = new Airplane();
+            //moveAirplaneOnAir.Backward(); // Airplane Move Backward on Air
+            //moveAirplaneOnAir.Forward();  // Airplane Move Forward
+            //moveAirplaneOnAir.Left();     // NotImplementedException
+            //moveAirplaneOnAir.Right();    // NotImplementedException
+
+
+            //IMoveOnGround moveAirplaneOnGround = new Airplane();
+            //moveAirplaneOnGround.Backward(); // Airplane Move Backward on Ground
+            //moveAirplaneOnGround.Forward();  // Airplane Move Forward
+            //moveAirplaneOnGround.Left();     // NotImplementedException
+            //moveAirplaneOnGround.Right();    // NotImplementedException
+
+            #endregion
         }
     }
 }
