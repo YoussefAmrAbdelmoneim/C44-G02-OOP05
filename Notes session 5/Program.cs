212e401cf9e9e1850a6a7670bbc95c16a37d850a
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-08-10 09:01:36 +0300

    Interface

--- Notes session 5/Program.cs
+++ Notes session 5/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Interface
+            // A top-level interface, one declared in a namespace but not nested inside another type, can be declared public or internal. 
+            //The default is internal. Nested interface declarations, those declared inside another type, can be declared using any access modifier.
+            //Interface members without an implementation can't include Private access modifier.
+            //Members with a default implementation can include any access modifier.
+            //Public access modifier is the default access modifier inside interface.
+            //You Can not Create an object from interface but you can create a reference from interface.
+            #endregion
+
         }
     }
 }
