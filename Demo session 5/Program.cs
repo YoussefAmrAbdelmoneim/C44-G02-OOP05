7cb1c84e6f430ac80d75478d61b03143699bee62
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-08-10 08:54:44 +0300

    Interface

--- Demo session 5/Program.cs
+++ Demo session 5/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Interface 
+            //IType refType;
+            //// Declare Reference From Type "IType"
+            //// This Reference Can Refer To An Object From Any Type That Implement Interface "IType"
+            //// CLR Will Allocate 4 Uninitialized Bytes At Stack 
+
+            ////refType = new IType(); // Invalid 
+
+            //refType = new Type();
+            //refType.MyProperty = 10;
+            //refType.MyMethod(); // Hello From Type
+            //refType.Print(); // Default Implemented Method
+
+
+            //Type typeObj = new Type();
+            //typeObj.MyProperty = 10;
+            //typeObj.MyMethod(); // Hello From Type
+            //					//typeObj.Print(); // Invalid 
+            #endregion
         }
     }
 }
